c2e5a48936e52452e1b99fa9f7559d17c49d5c78
Bobby Spencer <bbdgtl@gmail.com>
2017-11-27 14:04:07 +0100

    Complete functionality of Todo list

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,23 @@
 // Check Off specific Todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
 // Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+// Create new Todos
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    // grab new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    // create a new LI and add to UL
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
